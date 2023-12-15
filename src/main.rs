6ee1ac454913758518f2532c9a7d13e87e8af051
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2023-12-15 21:53:04 +0500

    Added `Printable` trait and implemented it for `Player` struct

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,14 @@ struct Player {
     score: u32,
 }
 
+trait Printable {
+    fn to_string(&self) -> String;
+}
+
+impl Printable for Player {
+    fn to_string(&self) -> String {
+        format!("{} ({})", self.name, self.score)
+    }
+}
+
 fn main() {}
